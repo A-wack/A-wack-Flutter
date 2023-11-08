41c3f4814995344fea31a56b9c4aee8a7c7fae37
ljyo2o9 <yggosxoljy@gmail.com>
2023-11-08 19:34:56 +0900

    a_wack_color 생성
    core/utils

---
+++ lib/src/core/utils/a_wack_color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AWackColor {
+  static const Color yellow = Color(0xFFFFA500);
+  static const Color gray = Color(0xFFF0F0F0);
+  static const Color black = Color(0xFF1E1E1E);
+  static const Color white = Color(0xFFFFFFFF);
+}
