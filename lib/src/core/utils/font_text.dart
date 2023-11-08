cc71b72e5b5bf28a8219b6b13770eefa381397f6
ljyo2o9 <yggosxoljy@gmail.com>
2023-11-08 16:43:41 +0900

    font_text.dart 수정
    text에 색깔을 넣을 수 있음

--- lib/src/core/utils/font_text.dart
+++ lib/src/core/utils/font_text.dart
@@ -5,64 +5,72 @@ import 'package:styled_widget/styled_widget.dart';
 String font = 'Pretendard';
 
 class PretendardFont {
-  Text thin(String text, num fontSize) {
+  Text thin(String text, num fontSize, Color color) {
     return Text(text).textStyle(TextStyle(
+      color: color,
       fontSize: fontSize.sp,
       fontFamily: font,
       fontWeight: FontWeight.w100,
     ));
   }
 
-  Text light(String text, num fontSize) {
+  Text light(String text, num fontSize, Color color) {
     return Text(text).textStyle(TextStyle(
+      color: color,
       fontSize: fontSize.sp,
       fontFamily: font,
       fontWeight: FontWeight.w300,
     ));
   }
 
-  Text regular(String text, num fontSize) {
+  Text regular(String text, num fontSize, Color color) {
     return Text(text).textStyle(TextStyle(
+      color: color,
       fontSize: fontSize.sp,
       fontFamily: font,
       fontWeight: FontWeight.w400,
     ));
   }
 
-  Text medium(String text, num fontSize) {
+  Text medium(String text, num fontSize, Color color) {
     return Text(text).textStyle(TextStyle(
+      color: color,
       fontSize: fontSize.sp,
       fontFamily: font,
       fontWeight: FontWeight.w500,
     ));
   }
 
-  Text semiBold(String text, num fontSize) {
+  Text semiBold(String text, num fontSize, Color color) {
     return Text(text).textStyle(TextStyle(
+      color: color,
       fontSize: fontSize.sp,
       fontFamily: font,
       fontWeight: FontWeight.w600,
     ));
   }
 
-  Text bold(String text, num fontSize) {
+  Text bold(String text, num fontSize, Color color) {
     return Text(text).textStyle(TextStyle(
+      color: color,
       fontSize: fontSize.sp,
       fontFamily: font,
       fontWeight: FontWeight.w700,
     ));
   }
 
-  Text extraBold(String text, num fontSize) {
+  Text extraBold(String text, num fontSize, Color color) {
     return Text(text).textStyle(TextStyle(
+      color: color,
       fontSize: fontSize.sp,
       fontFamily: font,
       fontWeight: FontWeight.w800,
     ));
   }
 
-  Text black(String text, num fontSize) {
+  Text black(String text, num fontSize, Color color) {
     return Text(text).textStyle(TextStyle(
+      color: color,
       fontSize: fontSize.sp,
       fontFamily: font,
       fontWeight: FontWeight.w900,
