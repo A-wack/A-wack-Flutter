8f11fbdf080c70b4f0f49f7a98c860f3dac9d2a9
ljyo2o9 <yggosxoljy@gmail.com>
2023-11-09 21:36:24 +0900

    a_wack_color 색 추가
    Color(0xFF909090)

--- lib/src/core/utils/a_wack_color.dart
+++ lib/src/core/utils/a_wack_color.dart
@@ -5,4 +5,5 @@ class AWackColor {
   static const Color gray = Color(0xFFF0F0F0);
   static const Color black = Color(0xFF1E1E1E);
   static const Color white = Color(0xFFFFFFFF);
+  static const Color more_gray = Color(0xFF909090);
 }
