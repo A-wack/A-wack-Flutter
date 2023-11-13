ac77d35741124ddf865327b7b49a37bd9b86e77e
ljyo2o9 <yggosxoljy@gmail.com>
2023-11-13 21:27:34 +0900

    tdd_list.dart 생성
    home_page

---
+++ lib/src/core/utils/tdd_list.dart
@@ -0,0 +1,12 @@
+final List<Map<String, dynamic>> applicationList = [
+  {"image": 'assets/image/tmp.png', "singTitle": 'title1', "choice": true},
+  {"image": 'assets/image/tmp.png', "singTitle": 'title2', "choice": true},
+  {"image": 'assets/image/tmp.png', "singTitle": 'title3', "choice": true},
+  {"image": 'assets/image/tmp.png', "singTitle": 'title4', "choice": true},
+  {"image": 'assets/image/tmp.png', "singTitle": 'title5', "choice": true},
+  {"image": 'assets/image/tmp.png', "singTitle": 'title6', "choice": true},
+  {"image": 'assets/image/tmp.png', "singTitle": 'title7', "choice": true},
+  {"image": 'assets/image/tmp.png', "singTitle": 'title8', "choice": true},
+  {"image": 'assets/image/tmp.png', "singTitle": 'title9', "choice": true},
+  {"image": 'assets/image/tmp.png', "singTitle": 'title10', "choice": true},
+];
