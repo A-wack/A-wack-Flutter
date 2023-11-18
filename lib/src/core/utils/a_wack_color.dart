4ced38ea43a64245dca9cdca312365da8fdc03eb
ljyo2o9 <yggosxoljy@gmail.com>
2023-11-18 21:36:49 +0900

    a_wack_color 수정
    gray_9090, gray_9999 생성

--- lib/src/core/utils/a_wack_color.dart
+++ lib/src/core/utils/a_wack_color.dart
@@ -5,6 +5,7 @@ class AWackColor {
   static const Color gray = Color(0xFFF0F0F0);
   static const Color black = Color(0xFF1E1E1E);
   static const Color white = Color(0xFFFFFFFF);
-  static const Color more_gray = Color(0xFF909090);
+  static const Color gray_9090 = Color(0xFF909090);
+  static const Color gray_9999 = Color(0xFF999999);
   static const Color red = Color(0xFFCA1B1B);
 }
